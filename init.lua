1959f654bcc7e72dcb5ca16040439539b0b4e8b0
Alex Murphy <alexander.murphy@orcro.co.uk>
2022-01-30 11:10:59 +0000

    with plugins

--- init.lua
+++ init.lua
@@ -4,7 +4,15 @@
 -- SPDX-Licence-Identifier: EPL-2.0
 
 
--- neovim options
+--[[ notes and resources
+
+https://vonheikemen.github.io/devlog/tools/configuring-neovim-using-lua/
+:h 
+
+]]--
+
+
+-- options
 
 vim.opt.number = true
 vim.opt.ignorecase = true
@@ -12,6 +20,8 @@ vim.opt.ignorecase = true
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2 vim.opt.softtabstop = 2 vim.wo.colorcolumn = "110"
+
+vim.cmd("colorscheme murphy")
 
 
 -- keybindings 
@@ -28,29 +38,63 @@ vim.api.nvim_set_keymap("n", "TT", ":terminal<Enter>:set nonumber<Enter>a", {nor
 vim.api.nvim_set_keymap("n", "BB", ":terminal<Enter>:set nonumber<Enter>arlwrap bb<Enter>", {noremap = true})
 
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", {noremap = true})
-vim.api.nvim_set_keymap("i", "(", "()<Left>", {noremap = true})
-vim.api.nvim_set_keymap("i", "[", "[]<Left>", {noremap = true})
-vim.api.nvim_set_keymap("i", "{", "{}<Left>", {noremap = true})
-vim.api.nvim_set_keymap("i", "\"", "\"\"<Left>", {noremap = true})
 vim.api.nvim_set_keymap("i", "<C-f>", "<Esc>A", {noremap = true})
 
 vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", {noremap = true})
 vim.api.nvim_set_keymap("t", "jj", "<C-\\><C-n>", {noremap = true})
-vim.api.nvim_set_keymap("t", "(", "()<Left>", {noremap = true})
-vim.api.nvim_set_keymap("t", "{", "{}<Left>", {noremap = true})
-vim.api.nvim_set_keymap("t", "[", "[]<Left>", {noremap = true})
-vim.api.nvim_set_keymap("t", "{", "{}<Left>", {noremap = true})
-vim.api.nvim_set_keymap("t", "\"", "\"\"<Left>", {noremap = true})
 vim.api.nvim_set_keymap("t", "<C-f>", "<Right><Right><Right><Right><Right><Right>", {noremap = true})
 
---
---[[ notes and resources
 
-https://vonheikemen.github.io/devlog/tools/configuring-neovim-using-lua/
+-- plugin manager
 
-]]--
+vim.cmd([[
+
+call plug#begin(stdpath('data') . '/plugged')
+
+Plug 'Olical/conjure'
+Plug 'jiangmiao/auto-pairs'
+
+call plug#end()
+
+]])
+
+
+-- leader keys
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+
+-- clerk
+
+vim.cmd([[
+function! ClerkShow()
+  exe "w"
+  exe "ConjureEval (nextjournal.clerk/show! \"" . expand("%:p") . "\")"
+endfunction
+
+nmap <silent> <localleader>cs :execute ClerkShow()<CR>
+]])
+
+-- currently unused
+
+-- doesn't seem necessary any more!
+--vim.cmd(setnocompatible)
+
+-- replaced by auto-pairs
+--vim.api.nvim_set_keymap("i", "(", "()<Left>", {noremap = true})
+--vim.api.nvim_set_keymap("i", "[", "[]<Left>", {noremap = true})
+--vim.api.nvim_set_keymap("i", "{", "{}<Left>", {noremap = true})
+--vim.api.nvim_set_keymap("i", "\"", "\"\"<Left>", {noremap = true})
+--vim.api.nvim_set_keymap("t", "(", "()<Left>", {noremap = true})
+--vim.api.nvim_set_keymap("t", "{", "{}<Left>", {noremap = true})
+--vim.api.nvim_set_keymap("t", "[", "[]<Left>", {noremap = true})
+--vim.api.nvim_set_keymap("t", "{", "{}<Left>", {noremap = true})
+--vim.api.nvim_set_keymap("t", "\"", "\"\"<Left>", {noremap = true})
+
 
 -- motivation
 
 print("Get cracking!")
+
 
