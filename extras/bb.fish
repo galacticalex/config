c22d14dcddfd4011bd93d332bb80a4f27965d85a
Alexander Murphy <alexander.murphy@orcro.co.uk>
2023-03-12 21:17:27 +0000

    fishfile copyright

--- extras/bb.fish
+++ extras/bb.fish
@@ -1,4 +1,12 @@
-# automatically include navigation in babashka repl
+
+# bb.fish
+#
+# SPDX-FileCopyrightText: 2023 Alexander Murphy <amsupernova@pm.me>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Fishscript alias to add history commands to babashka
+
 function bb
     rlwrap bb $argv
 end
